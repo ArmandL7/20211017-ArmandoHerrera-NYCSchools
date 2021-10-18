169db961dedf34d96994f484ed792eb3b9f7a88e
ArmandL7 <me@armandoherrera.com>
2021-10-18 10:02:03 -0400

    Initial Commit

---
+++ 20211017-ArmandoHerrera-NYCSchools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20211017-ArmandoHerrera-NYCSchools
+//
+//  Created by Armand on 18/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20211017-ArmandoHerrera-NYCSchools/_0211017_ArmandoHerrera_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0211017_ArmandoHerrera_NYCSchoolsApp.swift
+//  20211017-ArmandoHerrera-NYCSchools
+//
+//  Created by Armand on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct _0211017_ArmandoHerrera_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
